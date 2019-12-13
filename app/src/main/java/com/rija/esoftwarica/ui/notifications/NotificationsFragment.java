d70d5334de4ef39ecf574849d0acad6dca20192f
Rija Khatri <kcrijakhatri31@gmail.com>
2019-12-13 10:05:03 +0545

    Initial commit

---
+++ app/src/main/java/com/rija/esoftwarica/ui/notifications/NotificationsFragment.java
@@ -0,0 +1,41 @@
+package com.rija.esoftwarica.ui.notifications;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.rija.esoftwarica.R;
+
+public class NotificationsFragment extends Fragment {
+
+   WebView webview;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+//        notificationsViewModel =
+//                ViewModelProviders.of(this).get(NotificationsViewModel.class);
+        View view= inflater.inflate(R.layout.fragment_notifications, container, false);
+//        final TextView textView = root.findViewById(R.id.text_notifications);
+//        notificationsViewModel.getText().observe(this, new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+        webview=view.findViewById(R.id.webview);
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.setWebViewClient(new WebViewClient());
+        webview.loadUrl("https://softwarica.edu.np/");
+        return view;
+    }
+}
